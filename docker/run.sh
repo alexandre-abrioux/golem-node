419398e7c46ec5f01b5a2b30e80405ab37b6daa9
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2023-10-15 19:51:40 +0200

    comment

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-echo ">>> Running pre-install script"
-ya-provider pre-install
+# TODO: uncomment this after Yagna 0.13 upgrade
+# echo ">>> Running pre-install script"
+# ya-provider pre-install
 
 echo ">>> Importing resources"
 golemsp manifest-bundle add /usr/lib/yagna/installer/
